e7e85039e34073c07b01acfaf099fc657a47f763
tkzmkyms <tkzmkyms1002@gmail.com>
2025-05-17 09:57:53 +0900

    first_commit

---
+++ config/routes.rb
@@ -0,0 +1,22 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'prototypes/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+  root to: "prototypes#index"
+
+  resources :prototypes, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+
+  resources :prototypes do
+    resources :comments, only: :create
+  end
+
+  resources :users, only: [:show]
+
+end
